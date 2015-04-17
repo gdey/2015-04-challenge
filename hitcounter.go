00117096d0f9db9b8662106152a0a08aff0ab4bd
Gautam Dey <gautam.dey77@gmail.com>
2015-04-17 08:14:44 -0700

    Got the code working.

--- hitcounter.go
+++ hitcounter.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"image"
+	"image/color"
 	"image/draw"
 	"image/png"
 	"log"
@@ -16,8 +17,10 @@ var c counter.Value
 var mask *image.RGBA
 var maskCoords map[rune]image.Point
 
+const charwidth = 55
+
 func loadMask(path string) (m *image.RGBA, err error) {
-	f, err := os.Open("images/numbers.png")
+	f, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
@@ -32,23 +35,23 @@ func loadMask(path string) (m *image.RGBA, err error) {
 
 func initMask() {
 	var err error
-	mask, err = loadMask("image/numbersgbtp.png")
+	mask, err = loadMask("images/numberstp.png")
 	if err != nil {
 		panic(nil)
 	}
 	maskCoords = make(map[rune]image.Point)
-	maskCoords['1'] = image.Pt(0, 0)
-	maskCoords['2'] = image.Pt(100, 0)
-	maskCoords['3'] = image.Pt(200, 0)
-	maskCoords['4'] = image.Pt(0, 100)
-	maskCoords['5'] = image.Pt(100, 100)
-	maskCoords['6'] = image.Pt(200, 100)
-	maskCoords['7'] = image.Pt(0, 200)
-	maskCoords['8'] = image.Pt(100, 200)
-	maskCoords['9'] = image.Pt(200, 200)
-	maskCoords['0'] = image.Pt(100, 300)
-	maskCoords[','] = image.Pt(0, 300)
-	maskCoords['.'] = image.Pt(200, 300)
+	maskCoords['1'] = image.Pt(25, 0)
+	maskCoords['2'] = image.Pt(125, 0)
+	maskCoords['3'] = image.Pt(225, 0)
+	maskCoords['4'] = image.Pt(25, 100)
+	maskCoords['5'] = image.Pt(125, 100)
+	maskCoords['6'] = image.Pt(225, 100)
+	maskCoords['7'] = image.Pt(25, 200)
+	maskCoords['8'] = image.Pt(125, 200)
+	maskCoords['9'] = image.Pt(225, 200)
+	maskCoords['0'] = image.Pt(125, 300)
+	maskCoords[','] = image.Pt(25, 300)
+	maskCoords['.'] = image.Pt(225, 300)
 }
 
 func humanizeString(i int) string {
@@ -73,6 +76,30 @@ func humanizeString(i int) string {
 	stri = st
 	return string(stri)
 }
+func imageForNumber(i int, fg, bg color.Color) image.Image {
+	stri := humanizeString(i)
+	len := int(len(stri) * charwidth)
+	rec := image.Rect(0, 0, len, 100)
+	dest := image.NewRGBA(rec)
+	draw.Draw(dest, dest.Bounds(), &image.Uniform{bg}, image.ZP, draw.Src)
+	for j, d := range stri {
+		var coord image.Point
+		var f bool
+
+		if coord, f = maskCoords[d]; !f {
+			fmt.Println("We could not find ", d)
+		}
+		draw.DrawMask(
+			dest,
+			image.Rect(j*charwidth, 0, (j*charwidth)+charwidth, 100),
+			&image.Uniform{fg},
+			image.ZP,
+			mask,
+			coord,
+			draw.Over)
+	}
+	return dest
+}
 
 func counterHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -84,23 +111,8 @@ func counterHandler() http.Handler {
 		switch r.Method {
 		case "GET":
 			i := c.Get(id)
-			stri := humanizeString(i)
-			len := int(len(stri) * 100)
-			rec := image.Rect(0, 0, len, 100)
-			destimag := image.NewRGBA(rec)
-			//blue := color.RGBA{0, 0, 255, 255}
-			for j, d := range stri {
-				var coord image.Point
-				var f bool
-
-				if coord, f = maskCoords[d]; !f {
-					fmt.Println("We could not find ", d)
-				}
-				draw.Draw(destimag, image.Rect(j*100, 0, (j*100)+100, 100), mask, coord, draw.Src)
-				//draw.DrawMask(destimag, image.Rect(j*100, 0, (j*100)+100, 100), &image.Uniform{blue}, coord, mask, coord, draw.Over)
-			}
-
-			err := png.Encode(w, destimag)
+			dest := imageForNumber(i, color.RGBA{0, 0, 255, 255}, color.RGBA{0, 255, 0, 255})
+			err := png.Encode(w, dest)
 			if err != nil {
 				http.Error(w, fmt.Sprintf("Internal error %s", err), http.StatusInternalServerError)
 				return
